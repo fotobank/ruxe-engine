677066f4e254c9cf8ac9f82b6e8b7726f85c581c
Denis Akhrameyev <maindefine@gmail.com>
2014-05-20 13:36:50 +1100

    Чистка

--- conf/backup.php
+++ conf/backup.php
@@ -1,8 +1,8 @@
 <?php
 			$backup_date="20.05.2014";
-			$backup_all_hits="167";
-			$backup_all_hosts="4";
-			$backup_downloads="keybook=3|ноутпад=4";
-			$backup_views="test=8";
+			$backup_all_hits="0";
+			$backup_all_hosts="0";
+			$backup_downloads="keybook=0|ноутпад=0";
+			$backup_views="test=0";
 			$backup_links="";
 			
