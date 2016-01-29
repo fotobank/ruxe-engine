632d05cb9b206de2e772658408ff7c5e16462739
Denis <maindefine@yandex.ru>
2016-01-29 15:02:37 +1000

    Актуализация

--- conf/backup.php
+++ conf/backup.php
@@ -1,8 +1,8 @@
 <?php
-			$backup_date="20.05.2014";
-			$backup_all_hits="0";
-			$backup_all_hosts="0";
-			$backup_downloads="keybook=0|ноутпад=0";
-			$backup_views="test=0";
+			$backup_date="29.01.2016";
+			$backup_all_hits="29";
+			$backup_all_hosts="1";
+			$backup_downloads="keybook=0|android=0|backuper=0|sot=0";
+			$backup_views="test=1";
 			$backup_links="";
 			
