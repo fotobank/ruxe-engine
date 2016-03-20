2799942454cc2a6959f20172c929024223caf585
Denis Akhrameyev <contact@ahrameev.ru>
2016-03-20 15:33:27 +1000

    fix

--- conf/backup.php
+++ conf/backup.php
@@ -1,8 +1,8 @@
 <?php
-			$backup_date="29.03.2016";
-			$backup_all_hits="29";
-			$backup_all_hosts="1";
+			$backup_date="20.03.2016";
+			$backup_all_hits="0";
+			$backup_all_hosts="0";
 			$backup_downloads="keybook=0|android=0|backuper=0|sot=0";
-			$backup_views="test=1";
+			$backup_views="test=0";
 			$backup_links="";
 			
